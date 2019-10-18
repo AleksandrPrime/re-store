556ef09f0b62ff0ee8ba58e6afb9323cbd663532
AlexPrime <pochtarev.dev@gmail.com>
2019-10-19 00:57:25 +0300

    add links in index.html, compose in book-list

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
+import { compose } from '../../utils'
 
+import { withBookstoreService } from '../hoc';
+import { booksLoaded } from '../../actions';
 import './book-list.css'
 
 class BookList extends Component {
+
+    componentDidMount() {
+        //1. receive data
+        const { bookstoreService } = this.props;
+        const data = bookstoreService.getBooks();
+
+        //2. dispatch action to store
+        this.props.booksLoaded(data);
+    }
+
     render() {
         const { books } = this.props;
         return (
-            <ul>
+            <ul className="book-list">
                 {
                     books.map((book) => {
                         return (
@@ -25,4 +38,11 @@ const mapStateToProps = ({ books }) => {
     return { books };
 };
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = {
+    booksLoaded
+};
+
+export default compose(
+    withBookstoreService(),
+    connect(mapStateToProps, mapDispatchToProps))(BookList)
+
